e8c1ce7cd3886fa56c11e9dd53ca092d67992e12
nhaatjminh <minhphan.240199@gmail.com>
2022-04-23 01:25:10 +0700

    set up cookie

---
+++ src/helpers/login.js
@@ -0,0 +1,26 @@
+import { deleteAllCookies, deteletAllCookie, setCookie } from './cookie';
+
+const urlParams = new URLSearchParams(window.location.search);
+const tokenQuery = urlParams.get('token');
+
+export const login = (token) => {
+    deleteAllCookies();
+
+    // const domain = process.env.COOKIE_DOMAIN;
+    const domain = 'localhost';
+
+    setCookie(365, token, 'token', domain);
+
+    window.location.replace('/store-login');
+};
+
+export const logout = () => {
+    // const domain = process.env.COOKIE_DOMAIN;
+    const domain = 'localhost'
+
+    deteletAllCookie(domain);
+
+    if (!tokenQuery) {
+        window.location.replace('/login');
+    }
+};
