9fe2b10fcfebdf827b4351c9e33e2aa774aa2edf
nhaatjminh <minhphan.240199@gmail.com>
2022-04-26 00:35:33 +0700

    fix view onlin store

--- src/pages/OnlineStore/ManageThemes/index.js
+++ src/pages/OnlineStore/ManageThemes/index.js
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { ViewIcon } from '../../../assets/icon/ViewIcon';
 import './index.css'
 import HeaderDetailStore from './../../../component/HeaderDetailStore/index';
 import NavBarDetailStore from "../../../component/NavBarDetailStore";
 import { useParams } from 'react-router-dom';
+import { StoreApi } from "../../../service/api";
 
 const ManageThems = () => {
     const params = useParams();
-    
+    const [homePageId, setHomePageId] = useState('');
+
+    useEffect(() => {
+        const query = {
+            name: 'Home'
+        }
+        
+        StoreApi.getPagesByStoreId(params.storeId, query)
+            .then((result) => setHomePageId(result.data[0].id))
+            .catch(err => console.log(err));
+
+    }, [])
+
     const goToEditor = () => {
-        window.open(process.env.REACT_APP_EDITOR_URL + `/editor/${params.storeId}`);
+        window.open(process.env.REACT_APP_EDITOR_URL + `/editor/${params.storeId}?pageId=${homePageId}`);
     }
 
     return (
@@ -23,7 +36,7 @@ const ManageThems = () => {
                     <div className="mamagethemes__header">
                         <div className="mamagethemes__header--title">Theme</div>
                         <div className="mamagethemes__header--btn-to-editor" onClick={goToEditor}>
-                            <span style={{paddingRight: '5px'}}>
+                            <span style={{ paddingRight: '5px' }}>
                                 <ViewIcon />
                             </span>
                             <span>
--- src/service/api/storeApi.js
+++ src/service/api/storeApi.js
@@ -8,5 +8,18 @@ export const StoreApi = {
     createStore: async (storeObj) => {
         const result = await callAPIWithPostMethod('stores', storeObj, true);
         return result;
-    }
+    },
+    getPagesByStoreId: async (id, query) => {
+        let list_query = '';
+        let url = `stores/${id}/pages`
+        for (let key in query) {
+            list_query = list_query + '&' + key + '=' + query[key];
+        }
+        if (list_query.length > 0) {
+            list_query = list_query.slice(1);
+            url = url + '?' + list_query;
+        }
+        const result = await callAPIWithGetMethod(url, true);
+        return result;
+    },
 }
