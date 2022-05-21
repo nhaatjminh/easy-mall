7e265f24f2e2aaaef02eac95254b4fabb275e068
nhaatjminh <minhphan.240199@gmail.com>
2022-05-21 19:41:13 +0700

    add inside logout & validate page name

--- src/service/api/pageApi.js
+++ src/service/api/pageApi.js
@@ -5,6 +5,10 @@ export const PageApi = {
         const result = await callAPIWithGetMethod(`stores/${storeId}/pages`, true);
         return result;
     },
+    checkExistName: async (name, storeId) => {
+        const result = await callAPIWithGetMethod(`stores/${storeId}/page/${name}`, false);
+        return result;
+    },
     createPage: async (pageObj) => {
         const result = await callAPIWithPostMethod('pages', pageObj, true);
         return result;
