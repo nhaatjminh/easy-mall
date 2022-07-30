9fd14991e9258228e17784bb7d4b7a3bcb5f51a4
nhaatjminh <minhphan.240199@gmail.com>
2022-07-30 19:55:02 +0700

    add store logo

--- src/helpers/common.js
+++ src/helpers/common.js
@@ -8,7 +8,21 @@ export const checkValidURL = (data) => {
 }
 
 export const getDateTime = (date, time) => {
-    const __date = new Date(new Date(date) - 7*3600000)
-    const __time = new Date(new Date(time) - 7*3600000)
+    const __date = new Date(new Date(date) - 7 * 3600000)
+    const __time = new Date(new Date(time) - 7 * 3600000)
     return __date.toISOString().split('T')[0] + ' ' + __time.toLocaleTimeString();
 }
+
+export const getBase64 = (file) => {
+    return new Promise(resolve => {
+        let reader = new FileReader();
+
+        // Convert the file to base64 text
+        reader.readAsDataURL(file);
+
+        // on reader load something...
+        reader.onload = () => {
+            resolve(reader.result);
+        };
+    });
+};
